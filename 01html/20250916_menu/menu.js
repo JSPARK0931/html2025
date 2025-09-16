db644c22920cc3052259d7f3d840140605ef9061
JSPARK0931 <nosmoker@naver.com>
2025-09-16 14:32:31 +0900

    menu

---
+++ 01html/20250916_menu/menu.js
@@ -0,0 +1,26 @@
+const menu = [
+  {
+    //menu[0].menu
+    menu: "company",
+    url: "#",
+  },
+  {
+    //menu[1].menu
+    menu: "product",
+    url: "#",
+    //        sub:[ {smenu},{},{}]
+  },
+  {
+    //menu[2].menu
+    menu: "online",
+    url: "#",
+  },
+  {
+    menu: "community",
+    url: "#",
+  },
+  {
+    menu: "board",
+    url: "#",
+  },
+];
