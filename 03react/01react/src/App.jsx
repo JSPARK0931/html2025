d7e4d8f9ab091c6e229ff7e2441082214afd176e
JSPARK0931 <nosmoker@naver.com>
2025-10-16 14:22:35 +0900

    10/16 react

--- 03react/01react/src/App.jsx
+++ 03react/01react/src/App.jsx
@@ -1,51 +1,42 @@
 import React from "react";
-import "./App.css";
+import react from "./assets/react.svg";
 
 function App() {
-  const viewText = "안녕하세요";
+  const test = 10;
+  const view = {
+    color: "white",
+    backgroundColor: "pink",
+  };
+  let isLogin = true;
+
   const city = ["서울", "부산", "대구", "광주"];
   return (
+    // JSX문법 div는 하나만 허용함
+    // class => className
+    // 두개이상시 pragnant이용
+    // <>
+    //   <div className=''>
+    //     <h3>TEST</h3>
+    //     <p>{test}</p>
+    //     <div className="container"></div>
+    //   </div>
+    // </>
     <>
-      <div className="container bg-primary text-white">{viewText}</div>
-      <h3>{viewText}</h3>
-      <img src="./vita.svg" alt="" />
-      <div className="row">
-        {city.map((item, index) => {
-          return (
-            <>
-              <div className="col">{item}</div>
-            </>
-          );
-        })}
-
-        {city.map((item, index) => {
-          console.log("out===> ", item, index);
-          return (
-            <div className="col" key={index}>
-              {item}
-            </div>
-          );
-        })}
-
-        {city.map((item, index) => {
-          return (
-            <>
-              <div className="col">{item}</div>
-            </>
-          );
-        })}
-
-        {/* {city.map(()=>{return(<div></div>)})} */}
-        {/* {city.map(item)=>{
-          return (
-            <div className ="col">{item}</div>
-          )
-        }} */}
-        {/* <div className="col">1</div>
-        <div className="col">2</div>
-        <div className="col">3</div>
-        <div className="col">4</div> */}
+      <div className="container bg-primary" style={view}>
+        1111 <br />
+        <img src={react} alt="" />
+        {isLogin ? <p>로그인완료</p> : <p>로그인하세요</p>}
+        {test == 10 ? <p>로그인완료</p> : <p>로그인하세요</p>}
       </div>
+      {city.map((item, index) => {
+        return (
+          <>
+            <div key={index}>
+              {item} : {index}
+            </div>
+          </>
+        );
+      })}
     </>
   );
 }
---
+++ 03react/02react/src/App.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import Button from "./components/Button";
+import ViewComp from "./components/ViewComp";
+import View01 from "./components/View01";
+
+function App() {
+  const city = ["서울", "대전", "인천", "춘천"];
+  const city1 = ["서울", "제주", "인천", "춘천"];
+  const city2 = [
+    { title: "서울", content: "aaaa" },
+    { title: "대전", content: "bbbb" },
+    { title: "인천", content: "cccc" },
+    { title: "춘천", content: "dddd" },
+  ];
+
+  return (
+    <div>
+      App
+      <Button title="글작성" color="red" />
+      <Button title="글보기" color="blue" />
+      <Button title="글수정" color="pink" />
+      <Button title="글수정" color="black" />
+      <ViewComp cityData={city} />
+      <ViewComp cityData={city1} />
+      {city.map((item, i) => {
+        return <View01 title={item}></View01>;
+      })}
+      <Han />
+    </div>
+  );
+}
+
+function Han() {
+  return (
+    <>
+      <div>han</div>
+    </>
+  );
+}
+
+export default App;
