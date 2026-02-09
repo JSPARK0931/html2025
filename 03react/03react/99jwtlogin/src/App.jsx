17e56f15ebf3e44f07d8af36e090f265d6434581
JSPARK0931 <nosmoker@naver.com>
2026-02-09 13:13:40 +0900

    02/09 jwtlogin

---
+++ 03react/03react/99jwtlogin/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { cn } from "./lib/utils";
+
+function App() {
+  return (
+    <div>
+      <div className={cn("bg-red-500")}>
+        <h1>Hello World</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
